aa7471fb67304fa9a45583141de6c6718c9a28ab
abu18sam <abd.samad18@gmail.com>
2022-10-04 12:58:58 +0530

    Removed reference

--- src/components/headingAndActionsModal/HeadingAndActionsModal.jsx
+++ src/components/headingAndActionsModal/HeadingAndActionsModal.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect } from 'react';
 import PropTypes from "prop-types";
 
 // import BaseModal from "../baseModal/BaseModal";
